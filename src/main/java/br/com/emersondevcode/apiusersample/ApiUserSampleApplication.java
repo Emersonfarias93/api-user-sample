3b08d3e5fed65391af840e6ce284d60846174908
Emerson de Farias Santos <emerson.farias@unimedjp.com.br>
2024-12-22 14:06:06 -0300

    Initial commit

---
+++ src/main/java/br/com/emersondevcode/apiusersample/ApiUserSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.emersondevcode.apiusersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emersondevcode/apiusersample/ApiUserSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emersondevcode.apiusersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
